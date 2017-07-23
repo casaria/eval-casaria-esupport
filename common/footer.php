af0eb3e0e0752b3918ccc1a7663cf682c16fcbe3
Peter Horrack <phorrack@casaria.net>
2017-07-23 13:32:07 -0700

    Icon credits

--- common/footer.php
+++ common/footer.php
@@ -34,9 +34,9 @@
 
 echo "<br><center><font size=0 color=$theme[text] face=\"$theme[font]\">$helpdesk_name<br>";
 echo "$lang_powered Peter & <b><a href=\"https://odoo.casaria.net\">TheTeam</a></b> v$version<br>";
-echo "<a href=\"https://icons8.com/icon/44052/Shopping-Cart-Loaded\">Some icon credits</a><br>";
+echo "<a href=\"https://icons8.com/icon/44052/Shopping-Cart-Loaded\"> some icon credits</a><br>";
 if($enable_stats == 'On'){
-	$mtime2 = explode(" ", microtime());
+	$mtime2 = explode(" ", microtime())
 	$endtime = $mtime2[0] + $mtime2[1];
 	$totaltime = $endtime - $starttime;
 	$totaltime = number_format($totaltime, 7);
