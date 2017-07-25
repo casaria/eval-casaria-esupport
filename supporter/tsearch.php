2fe17bf588afbed202cfc13158b08a30dcb99f02
Peter Horrack <phorrack@casaria.net>
2017-07-25 13:37:30 -0700

    style.php updated

--- supporter/tsearch.php
+++ supporter/tsearch.php
@@ -516,7 +516,7 @@ if(isset($search) || isset($s)) {
 						<TR>
 						<TD class=back2 align=right width=27%>'.$lang_ticket.' '.$lang_billingStatus.': </td>
 						<td class=back><select name=billingstatus>';
-   							 createBillingStatusMenu(1);
+							createStatusMenu(1);
 	echo '				<option value=initial>'.$lang_bs_initial.'</option>
 
 						</select>
