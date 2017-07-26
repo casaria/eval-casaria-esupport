0b71310615850c77891092f496c7066ceb620aa8
Peter Horrack <phorrack@casaria.net>
2017-07-26 14:43:29 -0700

    BUGS

--- common/login.php
+++ common/login.php
@@ -38,6 +38,7 @@ $mtime1 = explode(" ", microtime());
 $starttime = $mtime1[0] + $mtime1[1];
 
 require_once "../common/common.php";
+
 if(eregi("supporter", $PHP_SELF) || eregi("admin", $PHP_SELF)) {
    require_once "../lang/$default_language.lang.php"; 
    $login_logo ="../images/casariadefault/small-header-brown.gif";
