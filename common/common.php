1ffc2bcbb5aba7fc81e95ecb96d5a83a62f1dde7
Peter Horrack <phorrack@casaria.net>
2017-07-26 14:17:14 -0700

    BUGS

--- common/common.php
+++ common/common.php
@@ -1601,7 +1601,6 @@ function displayTicket($result)
     $recordcount = 0;
     $csv_string = "";
     $closed_ts = 0;
-    $CS = null;
 
     while ($row = $db->fetch_array($result)) {
         $row_is_closed = 0;
