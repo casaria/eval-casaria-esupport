e2d9c866e114fe766c82dfa421c390ce0de7d03b
Peter Horrack <phorrack@casaria.net>
2017-07-26 00:12:49 -0700

    u

--- tools/UpdateClosedDate.php
+++ tools/UpdateClosedDate.php
@@ -16,7 +16,7 @@ $today = getdate();
 if(isset($search)) {
     //lets get the information ready to be passed to the displayTicket table.
 
-    $sql  = 'SELECT *  FROM `tickets` WHERE `status` = \'CLOSED\'';
+    $sql  = 'SELECT * FROM "$mysql_ticket" WHERE "status" = "CLOSED"';
 
     $result = $db->query($sql);
 
