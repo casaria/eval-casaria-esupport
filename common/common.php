4c13a381e0becec84aed6afacd320a9fffde4652
Peter Horrack <phorrack@casaria.net>
2017-07-26 16:19:26 -0700

    BUGS

--- common/common.php
+++ common/common.php
@@ -1778,10 +1778,7 @@ function displayTicket($result)
        }
        */
     endTable();
-    echo  "<table class=border cellSpacing=0 cellPadding=0 width=\"50%\" align=center border=0>	<tr> 
-			<td>";
-    echo "<tr>
-				<td class=back>" . str_pad($row['id'], 5, "0", STR_PAD_LEFT) . "</td>";
+    echo "<TD valign='top' width='80%' >"
     $summary = array("recordcount" => $recordcount, "remarks" => "list (CSV):", "tktlist" => $csv_string);
     echo "$lang_summary: $lang_recordcount $summary[recordcount] $summary[remarks]<BR> $summary[tktlist]";
     echo "</table>";
