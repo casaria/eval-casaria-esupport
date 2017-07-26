eca826882b22f8f5fd32e741f794818605306287
Peter Horrack <phorrack@casaria.net>
2017-07-26 12:47:34 -0700

    bug fixes

--- common/common.php
+++ common/common.php
@@ -1603,6 +1603,94 @@ function displayTicket($result)
     $closed_ts = 0;
 
     while ($row = $db->fetch_array($result)) {
+
+        $last_update = $row['lastupdate'];  //last update timestamp.
+
+        echo "<tr>
+				<td class=back>" . str_pad($row['id'], 5, "0", STR_PAD_LEFT) . "</td>";
+        if (isAdministrator($cookie_name)) {
+            echo "<td class=back2><a href=\"" . $admin_site_url . "/control.php?t=users&act=uedit&id=" . getUserID($row['supporter']) . "\">" . $row['supporter'] . "</td>";
+        } else {
+            echo "<td class=back2><a href=\"index.php?t=memb&mem=" . $row['supporter'] . "\">" . $row['supporter'] . "</td>";
+        }
+        echo "<td class=\"back\">";
+        echo stripslashes($row['equipment']) . "</td>";
+
+
+        echo "<td class=\"back2\">";
+        echo "<a href=\"?t=tupd&id=" . $row['id'] . "\">";
+        echo stripslashes($row['short']) . "</a></td>
+			
+				<td class=back>" . $row['user'] . "</td>";
+        $grp_name = 'NONE';
+        $resultgroup = $db->query($sql3);
+        while ($row2 = $db->fetch_array($resultgroup)) {
+            if ($row2['id'] == $row['ugroupid']) {
+                $grp_name = $row2['group_name'];
+            }
+        }
+
+        echo "<td class=back2>" . $grp_name . "</td>
+
+				<td class=back>";
+
+        switch ($row['priority']) {
+            case ("$highest_pri"):
+                echo "<font color=red><b>" . $row[priority] . "</b></font>";
+                break;
+            case ($second):
+                echo "<b>" . $row[priority] . "</b>";
+                break;
+            default:
+                echo $row[priority];
+                break;
+        }
+
+        echo "</td>
+				<td class=back2> " . date("m/d/y", $row[create_date]) . "</td>";
+        echo "<td class=back> " . date("m/d/y", $row[lastupdate]) . "</td>";
+
+        //cookie_name='.$cookie_name.'
+        echo "<td class=back>";
+        $resultBStatus = $db->query($sqlBS);
+        while ($row2 = $db->fetch_array($resultBStatus)) {
+            if ($row2['id'] == $row['BILLING_STATUS']) {
+                $bsIconRef = $row2['icon_ref'];
+            }
+        }
+        echo '<a href="updatelog.php?&id=' . $row[id] . '" target="myWindow" onClick="window.open(\'\', \'myWindow\',
+					\'location=no, status=yes, scrollbars=yes, height=500, width=600, menubar=no, toolbar=no, resizable=yes\')">';
+
+        echo $row[status];
+        echo "</a></td>";
+        echo "<td class=back align=center><img height=28 src=\"../$theme[image_dir]$bsIconRef\"></td>";
+
+        $response = setResponse($last_update, $row[priority], $row[id]);
+
+        switch ($response) {
+            case('1'):
+                echo "<td class=back align=center><img height=20 src=\"../$theme[image_dir]hourglass1.gif\"></td>";
+                break;
+            case('2'):
+                echo "<td class=back align=center><img height=20 src=\"../$theme[image_dir]hourglass2.gif\"></td>";
+                break;
+            case('3'):
+                echo "<td class=back align=center><img height=20 src=\"../$theme[image_dir]hourglass3.gif\"></td>";
+                break;
+            case('4'):
+                echo "<td class=back align=center><img height=20 src=\"../$theme[image_dir]hourglass4.gif\"></td>";
+                break;
+            default:
+                echo "<td class=back align=center><img height=20 src=\"../$theme[image_dir]hourglass1.gif\"></td>";
+                break;
+        }
+
+        echo "</tr>";
+        $recordcount++;
+        $csv_string = $csv_string . $row[id] . ",";
+    }
+
+ /*  while ($row = $db->fetch_array($result)) {
         $row_is_closed = 0;
         $last_update = $row['lastupdate'];  //last update timestamp.
 		$cs = getHighestRank($mysql_status_table);
@@ -1695,6 +1783,7 @@ function displayTicket($result)
         $recordcount++;
         $csv_string = $csv_string . $row[id] . ",";
     }
+    */
     $summary = array("recordcount" => $recordcount, "remarks" => "list (CSV):", "tktlist" => $csv_string);
 	return $summary;
 }
