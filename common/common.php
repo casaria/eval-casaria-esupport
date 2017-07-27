5980a7d8e4e299616ad06e6960e42be7fa4b02a5
Peter Horrack <phorrack@casaria.net>
2017-07-27 14:34:11 -0700

    suppporter_site_url

--- common/common.php
+++ common/common.php
@@ -1782,7 +1782,7 @@ function displayTicket($result)
     endTable();
 
     $linkString= "<a href=$supporter_site_url/index.php?t=time&tids=\"$csv_string\">"."\"link to CSV list\"";
-    echo '<form name="formTimeTrack"  action="index.php?t=time" method=post>';
+    echo '<form name="formTimeTrack"  action="index.php?t=time" method=GET>';
     echo '<input type="hidden" name="t" value="time">';
 	echo '<input type="hidden" name="tids" value="$csv_string">';
     echo '<input type="hidden" value="$lang_printstats" name="hidemenu">';
