ece42e019c08faff3838f05184db481a9c1a700b
Peter Horrack <phorrack@casaria.net>
2017-09-03 02:43:22 -0700

    scripts.php added

--- common/scripts.php
+++ common/scripts.php
@@ -14,4 +14,4 @@ echo '<script src="../dylay/assets/js/main.js"></script>';
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" media="screen">
 <link href='http://fonts.googleapis.com/css?family=Titillium Web:300:400' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="assets/css/main.css" media="screen">
+<link rel="stylesheet" href="../assets/css/main.css" media="screen">
