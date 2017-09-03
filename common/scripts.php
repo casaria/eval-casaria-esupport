4b2cf74693c3c1c336b29227b3731f4d9d20d65d
Peter Horrack <phorrack@casaria.net>
2017-09-03 02:39:54 -0700

    scripts.php added

--- common/scripts.php
+++ common/scripts.php
@@ -6,9 +6,9 @@
  * Time: 2:01 AM
  */
 echo '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>';
-echo '<script src="assets/vendor/jquery.easing.1.3.js"></script>';
-echo '<script src="src/dylay.js"></script>';
-echo '<script src="assets/js/main.js"></script>';
+echo '<script src="dylay/assets/vendor/jquery.easing.1.3.js"></script>';
+echo '<script src="dylay/src/dylay.js"></script>';
+echo '<script src="dylay/assets/js/main.js"></script>';
 ?>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
