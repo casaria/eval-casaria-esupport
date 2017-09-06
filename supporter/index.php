5160f5596a4946ea90760f7bf3f0ef1644d39b91
Peter Horrack <phorrack@casaria.net>
2017-09-06 16:05:50 -0700

    endtable()  remove <BR>

--- supporter/index.php
+++ supporter/index.php
@@ -101,7 +101,7 @@ $enable_CloudControl = getCloudControlUserSetting($_SESSION['cookie_name']);
               <TBODY> 
               <TR> 
                 <TD vAlign=top width="180"> 
-                  <TABLE class=border cellSpacing=0 cellPadding=1 width="100%" 
+                  <TABLE class=border cellSpacing=0 cellPadding=0 width="100%" 
                   align=center border=0px>
                     <TBODY> 
                     <TR> 
