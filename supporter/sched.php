3bb36fd97d03e9b485b4972db9958118aa6c514e
Peter Horrack <phorrack@casaria.net>
2017-09-17 10:14:23 -0700

    4

--- supporter/sched.php
+++ supporter/sched.php
@@ -115,7 +115,7 @@ function listGroupMembers($group)
 
         echo "<div class=\"col-sm-6 ".($row[supporter] >= 1 ? "active" : "inactive")."\" username=\"$row[user_name]\">";
 							echo "<span style=height:60px;>";
-                            echo "$row[user_name] $row[first_name] $row[last_name]</span></div>";
+                            echo "$row[first_name] $row[last_name] <b>($row[user_name])</b><br>$row[email] $row[lastactive]</span></div>";
 
 
     }
