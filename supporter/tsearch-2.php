722dd09d3ef5a920f9a0722fb3c47f330be1c510
Peter Horrack <phorrack@casaria.net>
2017-10-02 17:33:31 -0700

    login cosmetic

--- supporter/tsearch-2.php
+++ supporter/tsearch-2.php
@@ -343,7 +343,7 @@ if(isset($search) || isset($s)) {
     } else {
         createHeader("$lang_searchresults SQL: " . $sql);
 
-        echo '<TABLE class=border cellSpacing=1 cellPadding=2 width="100%" align=center border=0>
+        echo '<TABLE class=alternate cellSpacing=1 cellPadding=2 width="100%" align=center border=0>
 				<TABLE cellSpacing=1 cellPadding=5 width="100%" border=0>';
         echo ' <tr> ';
 
